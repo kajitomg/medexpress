05f70742e70a16b13fe04ae7e7822e15a548b7b4
kajitomg <elementrixmg@bk.ru>
2025-09-03 03:48:03 +0300

    Настройка nginx под ssl и поддержку strapi с uploads

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: "https://med-ekspress.ru/panel",
   app: {
     keys: env.array("APP_KEYS"),
   },
