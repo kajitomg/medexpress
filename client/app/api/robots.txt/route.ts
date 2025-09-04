b6c96790bbdb21a39999a5e508533b2726561b76
kajitomg <elementrixmg@bk.ru>
2025-09-05 02:58:56 +0300

    Управление robots.txt через strapi

--- client/app/api/robots.txt/route.ts
+++ client/app/api/robots.txt/route.ts
@@ -1,11 +1,13 @@
 import { fetchSettings } from "@/entities/settings/services"
+import { NextResponse } from "next/server"
 
 export async function GET() {
   const response = await fetchSettings()
-
   const data = response.data
-
-  return new Response(data.robots_txt.url, {
-    headers: { "Content-Type": "text/plain" },
-  })
+  if (!data.robots_txt.url) {
+    return NextResponse.json({ status: 404 })
+  }
+  return NextResponse.redirect(
+    new URL(data.robots_txt.url, process.env.NEXT_PUBLIC_APP_URL)
+  )
 }
